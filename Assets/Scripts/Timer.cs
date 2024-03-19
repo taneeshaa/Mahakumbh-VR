e856a0b774debdf6223a3d93f19ced8606a5e597
taneeshaa <kalrataneesha@gmail.com>
2024-03-19 12:12:59 +0530

    Lights Added

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
-using Michsky.UI.Heat;
 
 public class Timer : MonoBehaviour , IPointerClickHandler 
 
